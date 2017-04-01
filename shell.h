c7b86e82747be7e20a0d052bece14d03dd752153
Aiman Abdullah Anees <aimanabdullah65@gmail.com>
2017-04-02 03:12:27 +0530

    Updated

--- shell.h
+++ shell.h
@@ -33,10 +33,33 @@ void launch_shell()
 		    {
 		    	help();
 		    }
+
+		    else if(strEql(ch,"multiply"))
+		    {
+			product();
+		    }
 		    
 		    else if(strEql(ch,"reverse-print"))
 		    {
 		    	reverse_print();
+		    }
+		    else if(strEql(ch,"factorial"))
+		    {
+			factorial();
+		    }
+		   
+                    else if(strEql(ch,"square"))
+		    {
+			square();
+		    }
+
+		    else if(strEql(ch,"cube"))
+		    {
+			cube();
+		    }
+		    else if(strEql(ch,"division"))
+		    {
+			division();
 		    }
 		    
 		    
@@ -53,6 +76,14 @@ void help()
 	print("\nreverse-print      : Reprint a given text in reverse order");
 	print("\necho               : Reprint a given text");
 	print("\nexit               : Quits the current shell");
+	print("\nmultiply           : Returns multiplication of n numbers");
+	print("\nfactorial          : Returns factorial of a number");
+	print("\nsquare             : Returns square of a number");
+	print("\ncube               : Returns cube of a number");
+	print("\ndivision           : Returns quotient");
+
+
+
 	
 	
 	print("\n\n");
@@ -81,6 +112,136 @@ void reverse_print()
 	print("\n");
 }
 
+void product()
+{
+	print("\nHow many numbers: ");
+	int n = str_to_int(readStr());
+	int i =0;
+	print("\n");
+	int arr[n];
+	fill_array(arr,n);
+	int s = product_array(arr,n);
+	print("Result: ");
+	print(int_to_string(s));
+	print("\n");
+}
+
+
+
+int product_array(int arr[],int n)
+{
+	int i = 0;
+	int s = 1;
+	for (i = 0;i<n;i++)
+	{
+		s *= arr[i];
+	}
+	return s;
+}
+
+int factorial_array(int arr,int n)
+{
+	int i = 1;
+	int s = 1;
+	for (i = 1;i<=arr;i++)
+	{
+		s *= i;
+	}
+	return s;
+}
+
+
+void fill_array(int arr[],int n)
+{
+	int i = 0;
+	for (i = 0;i<n;i++)
+	{
+		print(" ");
+		arr[i] = str_to_int(readStr());
+		print("\n");
+	}
+}
+
+int square_array(int arr,int n)
+{
+	int i=1;
+	int s=1;
+	for(i=1;i<=2;i++)
+	{
+		s=s*arr;
+	}
+	return s;
+}
+
+int cube_array(int arr,int n)
+{
+	int i=1;
+	int s=1;
+	for(i=1;i<=3;i++)
+	{
+		s=s*arr;
+	}
+	return s;
+}
+
+int division_array(int arr[],int n)
+{
+	int s=arr[0]/arr[1];
+	return s;
+}
+
+
+
+void factorial()
+{
+	int n=1;
+	int arr[n];
+	print("\n");
+	fill_array(arr,n);
+	int result=factorial_array(arr[0],n);
+	print("Result: ");
+	print(int_to_string(result));
+	print("\n");
+		
+}
+
+void square()
+{
+	int n=1;
+	int arr[n];
+	print("\n");
+	fill_array(arr,n);
+	int result=square_array(arr[0],n);
+	print("Result: ");
+	print(int_to_string(result));
+	print("\n");
+	
+}
+
+void cube()
+{
+	int n=1;
+	int arr[n];
+	print("\n");
+	fill_array(arr,n);
+	int result = cube_array(arr[0],n);
+	print("Result: ");
+	print(int_to_string(result));
+	print("\n");
+}
+
+void division()
+{
+	int n=2;
+	int arr[n];
+	print("\n");
+	fill_array(arr,n);
+	int result=division_array(arr,n);
+	print("Result: ");
+	print(int_to_string(result));
+	print("\n");
+}
+	
 
 
 #endif
