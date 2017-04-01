69a954738a13b2489bd57125ce85439b027962f8
Aiman Abdullah Anees <aimanabdullah65@gmail.com>
2017-04-01 12:57:26 +0530

    Updated

--- kernel.c
+++ kernel.c
@@ -1,7 +1,33 @@
-kmain(){
-	char* vidmem=(char*)0xb8000;
-	vidmem[0] = 'x';
-	vidmem[1] = 0x02;
-	
-	
+#include "include/screen.h"
+#include "include/kb.h"
+#include "include/string.h"
+#include "include/types.h"
+#include <stdio.h>
+
+
+kmain()
+{
+       clearScreen();
+       print("Welcome to JARVIS Operating System\n");
+       string condition = "1"; 
+       while (condition)
+       {
+                print("\nJARVIS> ");
+                
+                string ch = readStr();
+                if(strEql(ch,"clear"))
+                {
+                        clearScreen();
+                }
+                
+                else
+                {
+                        print("\nBad command!\n");
+                }
+                
+                print("\n");        
+       }
+	print("Good Buy :)");
+
+        
 }
